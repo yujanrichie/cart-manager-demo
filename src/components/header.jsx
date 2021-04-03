e7fbce34532e45b3490d605bbeab50e3739f54f3
Jan Richie Yu <yujanrichie@gmail.com>
2021-04-03 23:14:46 +0800

    install node-sass

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,15 +2,22 @@ import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-regular-svg-icons';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
-
+import './header.scss';
 
 class Header extends Component {
     render() {
         return (
-            <React.Fragment>
-                <FontAwesomeIcon icon={ faHeart } />
-                <FontAwesomeIcon icon={ faShoppingCart } />
-            </React.Fragment>
+            <div className="app-header container">
+                <div className="row">
+                    <div className="col-8">
+
+                    </div>
+                    <div className="col-4 icon-container">
+                        <FontAwesomeIcon icon={ faHeart } />
+                        <FontAwesomeIcon icon={ faShoppingCart } />
+                    </div>
+                </div>
+            </div>
         );
     }
 }
